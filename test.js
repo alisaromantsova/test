30ea3a681cbba727b823cad694058c09ffd4aa10
alisaromantsova <113140050+alisaromantsova@users.noreply.github.com>
2023-11-18 01:02:51 +0200

    1

--- test.js
+++ test.js
@@ -132,3 +132,63 @@
 //     alert("Ви скасували виконання.");
 //   }
 // );
+
+// --1--
+// 1) написати об*єкт студента який буде виводити ім*я, спеціальнісь, середній
+// бал і кількість пропущених занять
+// 2) написати метод який буде виводити цю інформацію
+// 3) написати три варіанти студентів
+// 4) прикріпити знначення за допомогою call apply bind
+// --2--
+// Написати дві кнопки і закріпити на них функції
+// при натисканні на кнопку html - має видати коротке визначення що це таке
+// при натисканні на кнопку css - має видати коротке визначення що це таке
+// --3--
+// Написати функцію магазин, яка отримує назву товару, ціну за кг і кількість товару
+// функція має повертати назву товару і вартість
+// перевірити на варіантах:
+// 1) banana 30, 4,5
+// 2) cherry 58, 1,3
+// 3) jrange 89. 3,4
+
+// const student1 = {
+//   name: "Ivan",
+//   specialty: "Management",
+//   marks: 89,
+//   skip: 22,
+//   showStudentInfo() {
+//     console.log("name " + this.name + " speciality: " + this.specialty + " marks: " + this.marks + " skipped: " + this.skip);
+//   },
+// };
+// student1.showStudentInfo();
+
+// const student2 = {
+//   name: "kirill",
+//   specialty: "Management zed",
+//   marks: 70,
+//   skip: 88,
+// };
+// student1.showStudentInfo.apply(student2);
+
+// student1.showStudentInfo.call(student2);
+
+// student1.showStudentInfo.bind(student2)();
+
+// const htmlRef = document.querySelector(".html-btn");
+// const cssRef = document.querySelector(".css-btn");
+
+// const htmlInfo = () => {
+//   console.log("Html - стандартизированный язык гипертекстовой разметки документов для просмотра веб-страниц в браузере");
+// };
+// const cssInfo = () => {
+//   console.log("CSS - — технология описания внешнего вида документа, преимущественно использующаяся как средство оформления веб-страниц");
+// };
+// htmlRef.addEventListener("click", htmlInfo);
+// cssRef.addEventListener("click", cssInfo);
+
+// const shop = (name, price, quantity) => {
+//   return console.log(`your ${name} cost ${(price * quantity).toFixed(2)}`);
+// };
+// shop("banana", 30, 4.5);
+// shop("cherry", 58, 1.3);
+// shop("jrange", 89, 3.4);
